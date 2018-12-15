1a29d4862e1cddc28d28912ffeb7a7357929022d
Kevin Tong <kevinsomnia17@gmail.com>
2018-12-15 10:11:52 -0500

    Line counter file selection. Imported modernizr. Some more style changes.

--- projects/line_counter/scripts/lc.js
+++ projects/line_counter/scripts/lc.js
@@ -1,5 +1,29 @@
-$(
-    function CountUp() {
-        console.log("counting");
+// Drag and drop
+var dropArea = document.getElementById('dropArea');
+
+dropArea.addEventListener('drop', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    console.log('Dropped');
+});
+
+updateFileList = function () {
+    var selector = document.getElementById('fileSelector');
+    var numFilesSelected = selector.files.length;
+
+    if (numFilesSelected == 0)
+        return;
+
+    var list = document.getElementById('fileList');
+
+    // Formatted list.
+    var listContents = '';
+
+    for (var i = 0; i < numFilesSelected; ++i) {
+        listContents += '<a class="file-list-item">' + selector.files.item(i).name + '</a>';
     }
-)
+
+    var label = '<strong>Selected Files:</strong>'
+    list.innerHTML = label + '<div class="list-group">' + listContents + '</div>';
+}
