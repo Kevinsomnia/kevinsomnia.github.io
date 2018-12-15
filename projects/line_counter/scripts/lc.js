f3170f9dd05be511b91c9a1a2936ee8a9762861e
Kevin Tong <kevinsomnia17@gmail.com>
2018-12-15 10:58:12 -0500

    Selected files accumulate instead of resetting.

--- projects/line_counter/scripts/lc.js
+++ projects/line_counter/scripts/lc.js
@@ -1,5 +1,6 @@
 // Drag and drop
 var dropArea = document.getElementById('dropArea');
+var files = [];
 
 dropArea.addEventListener('drop', function (e) {
     e.stopPropagation();
@@ -7,6 +8,22 @@ dropArea.addEventListener('drop', function (e) {
 
     console.log('Dropped');
 });
+
+addFiles = function () {
+    var selector = document.getElementById('fileSelector');
+    var numFilesSelected = selector.files.length;
+
+    // Add items to list (if it isn't already in the list).
+    for (var i = 0; i < numFilesSelected; i++) {
+        var fileName = selector.files.item(i).name;
+
+        if(!files.includes(fileName))
+            files.push(fileName);
+    }
+
+    // Update UI reprentation.
+    updateFileList();
+}
 
 updateFileList = function () {
     var selector = document.getElementById('fileSelector');
@@ -17,13 +34,16 @@ updateFileList = function () {
 
     var list = document.getElementById('fileList');
 
-    // Formatted list.
+    // Format list.
     var listContents = '';
 
-    for (var i = 0; i < numFilesSelected; ++i) {
-        listContents += '<a class="file-list-item">' + selector.files.item(i).name + '</a>';
+    for (var i = 0; i < files.length; ++i) {
+        listContents += '<a class="file-list-item">' + files[i] + '</a>';
     }
 
     var label = '<strong>Selected Files:</strong>'
     list.innerHTML = label + '<div class="list-group">' + listContents + '</div>';
+
+    // Update parallax.
+    jQuery(window).trigger('resize').trigger('scroll');
 }
