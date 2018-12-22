7b3b776bb25f87cc70806a3c6f8861484ebd0694
Kevin Tong <kevinsomnia17@gmail.com>
2018-12-21 22:18:04 -0500

    Removed the Controller class. Using a global-scope JSON object to hopefully fix things? WORK!

--- projects/soundsmash/scripts/soundsmash.js
+++ projects/soundsmash/scripts/soundsmash.js
@@ -1,66 +1,53 @@
 // Global variables.
-var scController;
-
-class SoundCloudController {
-    constructor() {
-        this.clientID = 'giRCTsKmvoxGF53IxQ6xEV1FzsR6IzQH';
-        this.streamUrl = null;
-        this.onRetrieved = null;
-        this.onFailed = null;
-
-        console.log('Initializing SC controller...');
-        console.log(this);
-
-        SC.initialize({client_id: this.clientID});
-    }
-
-    tryGetSound(link, onRetrieved, onFailed) {
-        // By default, set streamUrl to null. If everything is successful by the end, it will be an actual link!
-        this.streamUrl = null;
-
-        // Set callbacks for track retrieve result.
-        this.onRetrieved = onRetrieved;
-        this.onFailed = onFailed;
-
-        // Sanitize link.
-        link = link.trim();
-        console.log('start playing: ' + link);
-
-        // Resolve to get track ID from link.
-        SC.get('/resolve', { url: link }, this.onRetrievedTrack);
-    }
-
-    onRetrievedTrack(result) {
-        if(!result.errors) {
-            if(result.kind == 'track') {
-                // We need to provide the client ID to use the API and access the sound.
-                this.streamUrl = result.stream_url + '?client_id=' + this.clientID;
-                console.log('Got final stream URL: ' + streamUrl);
-
-                if(this.onRetrieved !== null) {
-                    this.onRetrieved();
-                }
-            }
-        }
-
-        if(this.onFailed !== null) {
-            if(result.errors) {
-                this.onFailed(result.errors[0].error_message);
-            }
-            else {
-                this.onFailed(result.kind + ' is unsupported');
-            }
-        }
-    }
-}
+var scController = {};
 
 function initController() {
-    scController = new SoundCloudController();
+    // Create controller object for this session.
+    console.log('Initializing SC controller...');
+    scController = { clientID:'giRCTsKmvoxGF53IxQ6xEV1FzsR6IzQH', streamUrl:null, onRetrieved:null, onFailed:null };
+    console.log(scController);
+
+    SC.initialize({ client_id: scController.clientID });
 }
 
-function play() {
+function tryGetSound(link, onRetrieved, onFailed) {
+    // By default, set streamUrl to null. If everything is successful by the end, it will be an actual link!
+    scController.streamUrl = null;
+
+    // Set callbacks for track retrieve result.
+    scController.onRetrieved = onRetrieved;
+    scController.onFailed = onFailed;
+
+    // Sanitize link.
+    link = link.trim();
+    console.log('start playing: ' + link);
+
+    // Resolve to get track ID from link.
+    SC.get('/resolve', { url: link }, function(result) {
+        if(!result.errors && result.kind == 'track') {
+            // We need to provide the client ID to use the API and access the sound.
+            scController.streamUrl = result.stream_url + '?client_id=' + scController.clientID;
+            console.log('Got final stream URL: ' + streamUrl);
+
+            if(scController.onRetrieved !== null) {
+                scController.onRetrieved();
+            }
+        }
+    
+        if(scController.onFailed !== null) {
+            if(result.errors) {
+                scController.onFailed(result.errors[0].error_message);
+            }
+            else {
+                scController.onFailed(result.kind + ' is unsupported');
+            }
+        }
+    });
+}
+
+function onPressPlay() {
     var link = $('#scLink').val();
-    scController.tryGetSound(link, onTrackLoad, onTrackFail);
+    tryGetSound(link, onTrackLoad, onTrackFail);
 }
 
 function onTrackLoad() {
