a544bd2a55a9ff664952a0dadc644cd0e7cee68c
Kevin Tong <kevinsomnia17@gmail.com>
2018-12-22 02:19:04 -0500

    First iteration of game rendering.

--- projects/soundsmash/scripts/soundsmash.js
+++ projects/soundsmash/scripts/soundsmash.js
@@ -1,10 +1,20 @@
-// Global variables.
+// HTML elements
+var gameCanvas = document.getElementById('gameView');
+
+// Audio controller variables.
 var scController = {};
+
+// Game variables.
+var curTime = 0.0;
+var lastTime = 0.0;
+var unitScale = 500.0;
+var offsetX = 0.0;
 
 function initController() {
     // Create controller object for this session.
     scController = { clientID: 'giRCTsKmvoxGF53IxQ6xEV1FzsR6IzQH', track: null, onRetrieved: null, onFailed: null };
     SC.initialize({ client_id: scController.clientID });
+
 }
 
 function getStreamUrl() {
@@ -66,7 +76,6 @@ function startSamplingTrack() {
 
     // Create audio context and buffer.
     var audioCtx = new AudioContext();
-    var bufferSrc = audioCtx.createBufferSource();
 
     // Get the audio data through AJAX.
     var request = new XMLHttpRequest();
@@ -75,16 +84,30 @@ function startSamplingTrack() {
 
     request.onload = function () {
         audioCtx.decodeAudioData(request.response, function (data) {
-            createBeatmap(data);
-
-            // Connect audio data to player and start playing.
-            bufferSrc.buffer = data;
-            bufferSrc.connect(audioCtx.destination);
-            bufferSrc.start();
+            initializeGame(audioCtx, data);
         }, null);
     }
 
     request.send();
+}
+
+function initializeGame(audioCtx, data) {
+    // Generate beatmap.
+    createBeatmap(data);
+
+    // Initialize time.
+    var now = new Date();
+    curTime = now.getTime();
+    lastTime = curTime;
+
+    // Connect audio data to player and start playing.
+    var bufferSrc = audioCtx.createBufferSource();
+    bufferSrc.buffer = data;
+    bufferSrc.connect(audioCtx.destination);
+    bufferSrc.start();
+
+    // Start drawing the game.
+    renderGame();
 }
 
 var peaks = []; // Array of samples indices.
@@ -92,7 +115,7 @@ var peaks = []; // Array of samples indices.
 function createBeatmap(data) {
     var numChannels = data.numberOfChannels;
 
-    if(numChannels != 2) {
+    if (numChannels != 2) {
         return; // Channel count is not supported.
     }
 
@@ -109,16 +132,56 @@ function createBeatmap(data) {
 function calculatePeaks(lChannel, rChannel, sampleRate) {
     var results = [];
     var dataLength = lChannel.length;
-    var stepSize = Math.ceil(0.01 * sampleRate); // Sample every 0.01 second interval.
+    var stepSize = Math.ceil(0.001 * sampleRate); // Sample every 0.001 second interval.
 
-    for(var i = 0; i < dataLength; i += stepSize) {
+    for (var i = 0; i < dataLength; i += stepSize) {
         var avgAmplitude = (lChannel[i] + rChannel[i]) * 0.5;
 
-        if(avgAmplitude > 0.5) {
+        if (avgAmplitude > 0.5) {
             console.log('peak: ' + i + ' / ' + dataLength);
             results.push(i);
         }
     }
 
     return results;
+}
+
+function renderGame() {
+    gameCanvas.width = window.innerWidth - 50;
+    gameCanvas.height = window.innerHeight - 200;
+
+    requestAnimationFrame(renderGame);
+    gameLoop();
+
+    var gameCtx = gameCanvas.getContext('2d');
+
+    gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+    gameCtx.lineWidth = 2;
+
+    // Draw vertical lines
+    var lineCount = Math.ceil(gameCanvas.width / unitScale);
+    gameCtx.strokeStyle = '#ffffff33'; // Translucent white.
+
+    for (var i = 0; i < lineCount; i++) {
+        gameCtx.beginPath();
+        var x = (i + 1) * unitScale;
+        gameCtx.moveTo(x + offsetX, 0);
+        gameCtx.lineTo(x + offsetX, gameCanvas.height);
+        gameCtx.stroke();
+    }
+}
+
+function gameLoop() {
+    var now = new Date();
+    curTime = now.getTime(); // TEMP. Should use the current audio sample for time reference.
+    var dt = 1 / 60.0; // Time delta (in seconds) between previous frame and this frame.
+
+    offsetX -= dt * unitScale;
+
+    if (offsetX < -unitScale) {
+        offsetX += unitScale; // Loop.
+    }
+
+    lastTime = curTime;
 }
