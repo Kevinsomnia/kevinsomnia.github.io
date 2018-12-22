10487230faa9cd78d0af8da6e43017626daf61be
Kevin Tong <kevinsomnia17@gmail.com>
2018-12-21 22:03:49 -0500

    Added load and fail callbacks. Worked around more weird errors.

--- projects/soundsmash/scripts/soundsmash.js
+++ projects/soundsmash/scripts/soundsmash.js
@@ -1,10 +1,12 @@
 // Global variables.
-var controller;
+var scController;
 
 class SoundCloudController {
     constructor() {
         this.clientID = 'giRCTsKmvoxGF53IxQ6xEV1FzsR6IzQH';
         this.streamUrl = null;
+        this.onRetrieved = null;
+        this.onFailed = null;
 
         console.log('Initializing SC controller...');
         console.log(this);
@@ -12,47 +14,59 @@ class SoundCloudController {
         SC.initialize({client_id: this.clientID});
     }
 
-    tryGetSound(link) {
+    tryGetSound(link, onRetrieved, onFailed) {
         // By default, set streamUrl to null. If everything is successful by the end, it will be an actual link!
         this.streamUrl = null;
+
+        // Set callbacks for track retrieve result.
+        this.onRetrieved = onRetrieved;
+        this.onFailed = onFailed;
 
         // Sanitize link.
         link = link.trim();
         console.log('start playing: ' + link);
 
         // Resolve to get track ID from link.
-        SC.get('/resolve', { url: link }, function(result) {
-            if(!result.errors) {
-                if(result.kind == 'track') {
-                    // We need to provide the client ID to use the API and access the sound.
-                    this.streamUrl = result.stream_url + '?client_id=' + this.clientID;
-                    console.log('Got final stream URL: ' + streamUrl);
-                    return true;
+        SC.get('/resolve', { url: link }, this.onRetrievedTrack);
+    }
+
+    onRetrievedTrack(result) {
+        if(!result.errors) {
+            if(result.kind == 'track') {
+                // We need to provide the client ID to use the API and access the sound.
+                this.streamUrl = result.stream_url + '?client_id=' + this.clientID;
+                console.log('Got final stream URL: ' + streamUrl);
+
+                if(this.onRetrieved !== null) {
+                    this.onRetrieved();
                 }
-                else {
-                    console.log('No support for ' + result.kind + '. Sorry!');
-                }
+            }
+        }
+
+        if(this.onFailed !== null) {
+            if(result.errors) {
+                this.onFailed(result.errors[0].error_message);
             }
             else {
-                // Error: most likely invalid client ID or track.
-                console.log('Cannot play track: ' + result.errors[0].error_message);
+                this.onFailed(result.kind + ' is unsupported');
             }
-        });
-
-        return false;
+        }
     }
 }
 
 function initController() {
-    controller = new SoundCloudController();
+    scController = new SoundCloudController();
 }
 
 function play() {
     var link = $('#scLink').val();
-    if(controller.tryGetSound(link)) {
-        console.log('Get sound success');
-    }
-    else {
-        console.log('Get sound failed');
-    }
+    scController.tryGetSound(link, onTrackLoad, onTrackFail);
+}
+
+function onTrackLoad() {
+    console.log('track loaded!');
+}
+
+function onTrackFail(errorMsg) {
+    console.log('track failed: ' + errorMsg);
 }
